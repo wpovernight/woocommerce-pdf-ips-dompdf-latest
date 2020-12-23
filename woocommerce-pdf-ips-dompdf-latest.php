5a9cb74938d8335022933072ef587b5fd39f0a38
Alexandre Faustino <alexmigf@gmail.com>
2020-12-23 11:24:23 +0000

    Replaces the upstream 'develop' branch for 'master' since the first one was already merged into master

--- woocommerce-pdf-ips-dompdf-latest.php
+++ woocommerce-pdf-ips-dompdf-latest.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce PDF Invoices & Packing Slips latest dompdf
  * Plugin URI: http://www.wpovernight.com
  * Description: Uses the latest release of dompdf instead of the legacy version bundled with the general release
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Ewout Fernhout
  * Author URI: http://www.wpovernight.com
  * License: GPLv2 or later
